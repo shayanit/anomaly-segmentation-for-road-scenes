25f3cd6fd89b63477da25c5a86942ce2399f025e
Shayan Taghinezhad Roudbaraki <shayan2370@gmail.com>
2024-05-19 18:55:05 +0200

    barlwotwins model modified

--- eval/BarlowTwinsModel.py
+++ eval/BarlowTwinsModel.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import torchvision.models as models
 
 barlowtwins_resnet = torch.hub.load('facebookresearch/barlowtwins:main', 'resnet50')
-for param in barlowtwins_resnet.parameters():
-    param.requires_grad = False
+# for param in barlowtwins_resnet.parameters():
+#     param.requires_grad = False
 barlowtwins_resnet = nn.Sequential(*list(barlowtwins_resnet.children())[:-2])
 num_classes = 20
 segmentation_head = nn.Conv2d(2048, num_classes, kernel_size=1)
