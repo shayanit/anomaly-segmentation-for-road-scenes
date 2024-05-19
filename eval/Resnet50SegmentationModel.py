0680914e8d5f19d77a5ebb2b990eedc003d61179
Shayan Taghinezhad Roudbaraki <shayan2370@gmail.com>
2024-05-19 23:27:22 +0200

    Resnet50SegmentationModel changed to load model state dict for final model

--- eval/Resnet50SegmentationModel.py
+++ eval/Resnet50SegmentationModel.py
@@ -4,12 +4,12 @@ import torchvision.models as models
 
 def resnet50SegmentationModel(model_state_dict = [], num_classes = 20):
     model = models.resnet50(pretrained=False)
-    if (model_state_dict):
-        model.load_state_dict(model_state_dict, strict=False)
     # for param in model.parameters():
     #     param.requires_grad = False #FREEZE
     model = nn.Sequential(*list(model.children())[:-2])
     segmentation_head = nn.Conv2d(2048, num_classes, kernel_size=1)
     upsample = nn.Upsample(size=(224,224), mode='bilinear', align_corners=False)
     final_model = nn.Sequential(model, segmentation_head, upsample)
+    if (model_state_dict):
+        final_model.load_state_dict(model_state_dict, strict=False)
     return final_model
